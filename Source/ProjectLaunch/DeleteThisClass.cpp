768cf888d6edf433f11262929bec10b95a96bf80
PxRx0xDxIxGxY <michael.adam.gilbert@gmail.com>
2016-09-12 20:30:18 -0700

    Initial Water

---
+++ Source/ProjectLaunch/DeleteThisClass.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ProjectLaunch.h"
+#include "DeleteThisClass.h"
+
+DeleteThisClass::DeleteThisClass()
+{
+}
+
+DeleteThisClass::~DeleteThisClass()
+{
+}
---
+++ Source/ProjectLaunch/ProjectLaunch.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ProjectLaunch.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectLaunch, "ProjectLaunch" );
